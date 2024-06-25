ab1bf358ee859d4349731755644f87d5ffbca5d2
Pierce-44 <piercehahn.coding@gmail.com>
2024-06-25 14:29:44 +0100

    added base url redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/drivers/hamilton", // on base url load default to showing Hamilton for now
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
