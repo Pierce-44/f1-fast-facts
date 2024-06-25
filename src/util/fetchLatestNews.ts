a080480def752a6c5c146d3fce2f60137b5d9974
Pierce-44 <piercehahn.coding@gmail.com>
2024-06-25 17:09:48 +0100

    added no cache to news fetch

--- src/util/fetchLatestNews.ts
+++ src/util/fetchLatestNews.ts
@@ -3,7 +3,12 @@ import { News } from "@/interfaces/interfaces";
 export async function fetchLatestNews(): Promise<News | null> {
   try {
     const response = await fetch(
-      "https://newsapi.org/v2/everything?q=Formula-1&language=en&apiKey=4d13c03e0d0f4806807fd2c0bb0d1975"
+      "https://newsapi.org/v2/everything?q=Formula-1&language=en&apiKey=4d13c03e0d0f4806807fd2c0bb0d1975",
+      {
+        headers: {
+          "Cache-Control": "no-store",
+        },
+      }
     );
 
     if (!response.ok) {
